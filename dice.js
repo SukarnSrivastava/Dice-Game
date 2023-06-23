0527cc6fcdff33421cf6a9859840a76b62253a73
Sukarn Srivastava <sukarnsrivastava@gmail.com>
2023-06-23 18:02:06 +0530

    1st commit

---
+++ dice.js
@@ -0,0 +1,14 @@
+function dice(){
+    let randomNumber1 = Math.floor(Math.random()*6) + 1;
+let randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+let randomImage1 = "/dice_challenge/dice_img/dice" + randomNumber1 + ".png"
+let image1 = document.getElementById("img1").setAttribute("src", randomImage1)
+
+let randomImage2 = "/dice_challenge/dice_img/dice" + randomNumber2 + ".png"
+let image2 = document.getElementById("img2").setAttribute("src", randomImage2)
+
+console.log(randomNumber1,randomNumber2);
+}
+let button = document.getElementsByClassName("btn")[0];
+button.addEventListener(("click"), e => dice());
